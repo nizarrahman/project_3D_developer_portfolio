14ef76cda0806bd689d826b6cc6de518b7e53e99
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-05-24 10:36:06 +0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Hallo Nama Saya Nizar, Saya kelas 2 otw kelas 3 smk, Umur saya sekarang 17 tahun, Saya seorang junior frond end Developer!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
